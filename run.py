b4119eab20eba20156bd4f7a413c68368419e188
bijokmathew <bijo.nbr@hotmail.com>
2022-09-13 19:58:18 +0000

    create the get_sales_data funtion to get the data in string format

--- run.py
+++ run.py
@@ -11,6 +11,15 @@ SCOPED_CREDS= CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT=gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
-print(data)
+def get_sales_data():
+    """
+    Get sales data from the user
+    """
+    print("Please enter the sales data from the last market")
+    print("Data should be six numbers seperated by commaa.")
+    print("Example : 10, 30, 60, 40, 90, 35 \n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
